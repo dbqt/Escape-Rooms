222816199a598b57ff9bc5932202315c7786b4a5
David Tran <dt_dbqt@hotmail.com>
2016-06-24 21:48:06 -0400

    basic game manager

---
+++ Escape Rooms/Assets/Code/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject[] Rooms;
+
+    private bool isPaused;
+
+	// Use this for initialization
+	void Start () 
+    {
+	   isPaused = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    //check if player is dead
+        // if(!player.GetComponent<>().)
+        {
+        //    EndLevel(false);
+        }
+        
+        //check if player has done level
+        //if((player.GetComponent<>().))
+        {
+        //  EndLevel(true);
+        }
+	}
+
+    public voiid TogglePause()
+    {
+        isPaused = !isPaused;
+        if(isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    private void EndLevel(bool isSuccess)
+    {
+        if(isSuccess)
+        {
+            LoadNextLevel();
+        }
+        else
+        {
+            LoadMenu();
+        }
+    }
+
+    private void LoadMenu()
+    {
+        //SceneManager.LoadScene("menu");
+    }
+
+    private void LoadNextLevel()
+    {
+        if(Rooms.Count <= 0) return; //abord!
+
+        int nextRoom = Random.Range(0, Rooms.Count);
+        SceneManager.LoadScene(Rooms[nextRoom].name);
+    }
+}
