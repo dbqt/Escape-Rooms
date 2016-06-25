5281ef76c0208ad53fb9befa90382f6f3240b365
mufasablasta <mustafaelsharif01@gmail.com>
2016-06-25 01:36:27 -0400

    Player Controller

---
+++ Escape Rooms/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float rotateSpeed;
+    private Rigidbody rb;
+    private float moveHorizontal;
+    private float moveVertical;
+
+    // Use this for initialization
+    void Start() {
+
+        moveHorizontal = 0;
+        moveVertical = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+
+        Debug.Log("Vertical : " + moveVertical);
+        Debug.Log("Horizontal : " + moveHorizontal);
+
+        if (moveVertical != 0.0)
+        {
+
+            
+            movement.z = moveVertical * (-1);
+            transform.Translate(movement * (speed * Time.deltaTime));
+
+
+        }
+        if (moveHorizontal >= 0.02 ||  moveHorizontal <= -0.02)
+        {
+          
+            transform.Rotate(new Vector3(0.0f, rotateSpeed * Mathf.Sign(moveHorizontal), 0.0f));
+            
+          
+        }
+      
+
+
+    }
+}
