4e180956e3c71b979f51b2e71d9b24008aa65342
David Tran <dt_dbqt@hotmail.com>
2016-06-25 07:43:34 -0400

    button reset properly

---
+++ Escape Rooms/Assets/Code/StartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+    public void StartAction()
+    {
+        GameManager.instance.StartNewGame();
+    }
+}
