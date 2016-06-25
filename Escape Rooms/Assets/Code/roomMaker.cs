1bea04caebcda450937cd1945d55ad10bd5ceb8d
Naly-chan <gracefulshawol@gmail.com>
2016-06-24 23:09:32 -0400

    added general roomMaker script

---
+++ Escape Rooms/Assets/Code/roomMaker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class roomMaker : MonoBehaviour {
+	public Transform startingPoint, endingPoint;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void reset(){
+		player.transform.position = startingPoint.position;
+	}
+}
