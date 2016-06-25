b93ae603c109364d22c016ca11fcb6709c46e590
David Tran <dt_dbqt@hotmail.com>
2016-06-24 22:56:21 -0400

    keeping stats

--- Escape Rooms/Assets/Code/GameManager.cs
+++ Escape Rooms/Assets/Code/GameManager.cs
@@ -4,20 +4,43 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+    static public GameManager instance = null;
+
     public GameObject player;
     public string[] Rooms;
 
     private bool isPaused;
+    private float timer;
 
 	// Use this for initialization
 	void Start () 
     {
-	   isPaused = true;
+        // singleton logic
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+       
+        isPaused = true;
+        PlayerPrefs.SetInt("nSuccess", 0);
+        PlayerPrefs.SetInt("nFail", 0);
+        Time.timeScale = 0f;
+        timer = 0f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
+        if(!isPaused)
+        {
+            timer += Time.deltaTime;
+        }
 	    //check if player is dead
         // if(!player.GetComponent<>().)
         {
@@ -48,21 +71,28 @@ public class GameManager : MonoBehaviour {
     {
         if(isSuccess)
         {
+            int successes = PlayerPrefs.GetInt("nSuccess");
+            PlayerPrefs.SetInt("nSuccess", successes+1);
             LoadNextLevel();
         }
         else
         {
+            int fails = PlayerPrefs.GetInt("nFail");
+            PlayerPrefs.SetInt("nFail", fails+1);
             LoadMenu();
         }
     }
 
     private void LoadMenu()
     {
+        TogglePause(); 
         //SceneManager.LoadScene("menu");
     }
 
     private void LoadNextLevel()
     {
+        TogglePause();
+        
         if(Rooms.Length <= 0) return; //abord!
 
         int nextRoom = Random.Range(0, Rooms.Length);
