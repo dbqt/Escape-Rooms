0b2744990d1f24ce61f397f431c2e5635eda5045
Naly-chan <gracefulshawol@gmail.com>
2016-06-25 15:28:15 -0400

    added new RandomTripleFallingFloor room

---
+++ Escape Rooms/Assets/Code/RandomTripleFallingFloor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomTripleFallingFloor : MonoBehaviour {
+	public GameObject fallingFloor1, fallingFloor2, fallingFloor3;
+	// Use this for initialization
+	void Start () {
+		fallingFloor1.transform.position = new Vector3(Random.Range(-2.5f, 2.5f), 0f, -2.55f);
+		fallingFloor2.transform.position = new Vector3(Random.Range(-2.5f, 2.5f), 0f, 0.08f);
+		fallingFloor3.transform.position = new Vector3(Random.Range(-2.5f, 2.5f), 0f, 2.84f);
+	}
+}
